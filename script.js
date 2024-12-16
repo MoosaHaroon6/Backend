d59a84a276827bcb06354bfa827a223229a9f712
Moosa Haroon <moosaharoon6nov@gmail.com>
2024-12-16 21:15:39 +0500

    Get learned

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
-let oneLineJoke = require('one-liner-joke');
+const express = require('express'); // express js installed
+let oneLineJoke = require('one-liner-joke');  // one-liner pkg 
+let figlet = require("figlet"); // figlet(pkg) from npmjs
+
+const port = 3000;
+const app = express(); // as app
+
 let joke = oneLineJoke.getRandomJoke().body;
+console.log(joke);
 
-console.log("This is Joke :::::", joke);
+let text = figlet("Hello World!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
+console.log(text);
 
 
+app.get('/', (req, res) => {
+  res.send('First Program! In backend it is running succesfully');
+})
 
-let figlet = require("figlet");
+app.get('/jokes', (req, res) => {
+  res.send(`<h1>Random Jokes</h1><p>${joke}</p>`);
+})
 
-// figlet.text(
-//     "Moosa",
-//     {
-//         font: "4Max",
-//         horizontalLayout: "default",
-//         verticalLayout: "default",
-//         width: 100,
-//         whitespaceBreak: true
-//     },
-//     function (err, data) {
-//         if (err) {
-//             console.log("Something went wrong...");
-//             console.dir(err);
-//             return;
-//         }
-//         console.log(data);
-//     })
+app.get('/text', (req, res) => {
+  figlet.text(
+    "Moosa",
+    {
+      font: "4Max",
+      horizontalLayout: "default",
+      verticalLayout: "default",
+      width: 100,
+      whitespaceBreak: true
+    },
+    function (err, data) {
+      if (err) {
+        res.send("Something went wrong...");
+        console.dir(err);
+        return;
+      }
+      res.send(`<pre>${data}</pre>`);
+    })
+})
 
-const express = require('express');
-const app = express();
+
+app.listen(port, console.log("First Program! In backend"));
