9d275e78e4f9b7ca0fe0480d2484e48f934097f9
Moosa Haroon <157456649+MoosaHaroon6@users.noreply.github.com>
2024-12-17 23:47:40 +0500

    Req and res / template engine

--- Day-02/script.js
+++ Day-02/script.js
@@ -2,17 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set("view engine","ejs"); // ejs configuration
+
 app.use((req, res, next) => {  // middleware function
   console.log("middleware request");
   next();
 }) 
 
 app.get('/', (req, res) => {
-  res.send("Hello world");
+  res.render("index");
 })
 
 app.get('/profile/:username', (req, res) => {
-  res.send(`Hey ${req.params.username}`);
+  res.send(`Hey ${req.params.username}`);  // dynamic routing
 })
 
 app.listen(port, console.log("Program Running"));
