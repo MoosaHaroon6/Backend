57ebe470f3abafdd4c2155096ad1761f2bf802fb
Moosa Haroon <157456649+MoosaHaroon6@users.noreply.github.com>
2025-01-17 00:30:30 +0500

    bcr

--- Day-07/index.js
+++ Day-07/index.js
@@ -3,10 +3,12 @@ import cors from "cors"
 import logger from "morgan"
 import "dotenv/config";
 import User from "./models/UserSchema.js";
-import { connection } from "./config/dbConfig.js";
+import bcrypt from 'bcrypt';
+import connection from "./config/dbConfig.js";
+import jwt from 'jsonwebtoken';
 
-const port = 3000;
 const app = express();
+connection();
 
 /* Middlewares */
 app.use(cors());
@@ -15,22 +17,59 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 /* Middlewares */
 
+const { PORT, secret_token } = process.env;
+
 app.get('/', (req, res) => {
     res.send("Hello world");
     console.log("Hello world")
 })
 
 app.post('/register', async (req, res) => {
-    const { email, username, password } = req.body;
-    const newUser = new User({ ...req.body });
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
     try {
         const saved = await newUser.save();
+        delete saved.password;
         console.log(saved, "User Saved");
     } catch (e) {
         console.error(e, "User Not created");
     }
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on Port ${port}`);
+app.post('/login', async (req, res) => {
+    try {
+
+        const user = await User.findOne({ email: req.body.email }); // user
+        if (!user) {
+            res.send({ message: "user not found" });
+            return;
+        }
+
+        const passwordMatch = await bcrypt.compare(req.body.password, user.password); // password
+        if (!passwordMatch) {
+            res.send({ message: "Invalid Password" });
+            return;
+        }
+
+        const userObj = user.toObject();
+        delete userObj.password;   // password removed for security 
+
+        const token = jwt.sign(
+            { id: user._id, email: user.email },
+            secret_token,
+            { expiresIn: "1hr" });
+
+        // res.send({ token: token, ...user });
+        // console.log("Token Got",token,{...user});
+
+        res.status(200).json({ token, user: userObj });
+
+    } catch (e) {
+        console.log("Error", e);
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}`);
 })
