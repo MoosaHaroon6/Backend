a5255973d55744acb6b44b4e81616b2804b510e0
Moosa Haroon <157456649+MoosaHaroon6@users.noreply.github.com>
2025-01-29 22:47:01 +0500

    edit / update feature added

--- Day-12/index.js
+++ Day-12/index.js
@@ -6,15 +6,19 @@ import User from './models/User.js'
 
 const app = express();
 
+// ejs
 app.set("view engine", "ejs");
 
+
+// middlewares
 app.use(cors("*"));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const { PORT } = process.env;
+const { PORT } = process.env; // env imports
 
+// routes
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -36,13 +40,31 @@ app.post("/create", async (req, res) => {
 
     res.redirect("/read");
     console.log(user);
-})
+});
 
 app.get("/delete/:id", async (req, res) => {
     const deleteUser = await User.findOneAndDelete({ _id: req.params.id });
     res.redirect("/read");
     console.log(deleteUser);
-})
+});
+
+app.get("/edit/:id", async (req, res) => {
+    const user = await User.findOne({ _id: req.params.id });
+    // console.log(user); 
+    res.render("edit", { user });
+});
+
+app.post("/update/:id", async (req, res) => {
+    const { name, email, image } = req.body;
+    const updatedUser = await User.findOneAndUpdate(
+        { _id: req.params.id }, // filteration id
+        { email, name, image }, // data to update
+        { new: true } // return updated document
+    );
+
+    console.log(updatedUser);
+    res.redirect("/read");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`);
